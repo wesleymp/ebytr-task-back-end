4d194eef7ca8099f46db2c1357c802763cd1988a
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2022-02-14 14:44:18 -0300

    test: criado o teste de integraçao para rota /tasks

---
+++ test/integration/allTask.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+
+const app = require('../../src/main/app');
+const { getConnection } = require('../../src/models/connection');
+
+describe('Rota para listar todas as tarefas (/tasks)', () => {
+  beforeEach(async () => {
+    const conn = await getConnection();
+    await conn.collection('tasks').insertOne({
+      title: 'valid_title_task',
+      status: 'pendente',
+      created_at: new Date().toLocaleString('pt-br', {
+        timeZone: 'America/Sao_Paulo',
+      }),
+      updated_at: new Date().toLocaleString('pt-br', {
+        timeZone: 'America/Sao_Paulo',
+      }),
+    });
+  });
+
+  it('deve retornar um status 404 se não tiver nenhuma tarefa criada', (done) => {
+    getConnection().then((conn) => {
+      conn.collection('tasks').deleteMany({});
+    });
+    request(app).get('/tasks').expect(404).end(done);
+  });
+
+  it('deve retornar um status 200 encontrar 1 ou mais tarefas', (done) => {
+    request(app).get('/tasks').expect(200).end(done);
+  });
+});
